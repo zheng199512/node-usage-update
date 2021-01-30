ab4ef1570f0a291254205507a66fa69b9f613fac
zheng199512 <911409649@qq.com>
2021-01-30 18:52:40 +0800

    init

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/zheng199512/node-usage-update
 
 go 1.14
 
-require k8s.io/klog v1.0.0
+require (
+	github.com/shirou/gopsutil v3.20.12+incompatible
+	github.com/shirou/gopsutil/v3 v3.20.12
+	k8s.io/klog v1.0.0
+)
--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
-	"github.com/zheng199512/node-usage-update/pkg/cpu"
+	"github.com/shirou/gopsutil/v3/cpu"
+	"k8s.io/klog"
 	"time"
 )
 
-func main() {
+func GetUsage() float64 {
+	usage, err := cpu.Percent(300, false)
+	if err != nil {
+		klog.Error("error get cpu percent")
+	}
+	return usage[0]
+}
 
+func main() {
 	for i := 0; i < 1; i++ {
-		usage := cpu.GetUsage()
-		println(usage)
+		test := GetUsage()
+		println(test)
 		time.Sleep(5 * time.Second)
 	}
 }
